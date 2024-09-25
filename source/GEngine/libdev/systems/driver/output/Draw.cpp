b0805642b1f26458e5ef9b471de9c55adb4dbd2b
Popochounet <adrien.audiard@epitech.eu>
2024-09-25 23:02:33 +0200

    added libdev: Component => Window, Shape, Color & System => RenderWindow, Draw

---
+++ source/GEngine/libdev/systems/driver/output/Draw.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-LYN-5-1-rtype-basile.fouquet
+** File description:
+** Draw.cpp
+*/
+
+#include "GEngine/libdev/systems/driver/output/Draw.hpp"
+#include <iostream>
+
+namespace gengine::system::driver::output {
+void Draw::init(void) {
+    subscribeToEvent<gengine::system::event::Draw>(&Draw::onDraw);
+}
+
+void Draw::onDraw(gengine::system::event::Draw &e) {
+    auto &rectangles = getComponent<component::driver::output::Rectangle>();
+    auto &colors = getComponent<component::driver::output::Color>();
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    for (auto &[entity, rect]: rectangles) {
+        if (colors.contains(entity))
+            DrawRectangle(rect.x, rect.y, rect.width, rect.height, colors.get(entity).color);
+    }
+    EndDrawing();
+}
+} // namespace gengine::system
