a3eb9cd0dea067c5c12dfa3e0f43e9f6dedb4255
b3W <basile.fouquet@epitech.eu>
2024-09-25 20:59:01 +0200

    added libdev: Systems => Motion2D/3D Collsion2D/3D & Components => Motion2D/3D Position2D/3D Origin2D/3D HitBoxCircle2D/3D HitBoxSuare2D/3D
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

--- r-type/client/main.cpp
+++ r-type/client/main.cpp
@@ -10,10 +10,53 @@
 #include "GEngine/interface/Internal.hpp"
 #include "GEngine/libdev/systems/MainLoop.hpp"
 
+#include "GEngine/libdev/systems/events/Native.hpp"
+
+#include "GEngine/libdev/System.hpp"
+
+#include "GEngine/libdev/components/HitBoxs.hpp"
+#include "GEngine/libdev/components/Motions.hpp"
+#include "GEngine/libdev/components/Positions.hpp"
+#include "GEngine/libdev/systems/Collisions.hpp"
+#include "GEngine/libdev/systems/Motions.hpp"
+
+class Start : public gengine::System<Start> {
+public:
+    void init(void) override { subscribeToEvent<gengine::system::event::StartEngine>(&Start::onStartEngine); }
+
+    void onStartEngine(gengine::system::event::StartEngine &e) {
+        spawnEntity(gengine::component::Position2D(0, 0), gengine::component::Motion2D(1, 0),
+                    gengine::component::HitBoxSquare2D(1, 1), gengine::component::Origin2D(0, 0));
+        spawnEntity(gengine::component::Position2D(10, 0), gengine::component::Motion2D(-1, 0),
+                    gengine::component::HitBoxSquare2D(1, 1));
+    }
+};
+
+#include "GEngine/libdev/systems/events/game/Collision.hpp"
+
+class HandleCollision : public gengine::System<HandleCollision> {
+public:
+    void init(void) override { subscribeToEvent<gengine::system::event::Collsion>(&HandleCollision::onCollision); }
+
+    void onCollision(gengine::system::event::Collsion &e) {
+        std::cout << "collision " << e.entity1 << "<->" << e.entity2 << std::endl;
+    }
+};
+
 int main(void) {
     gengine::game::Engine gameEngine;
     gengine::driver::Engine driverEngine;
 
+    gameEngine.registerComponent<gengine::component::Position2D>();
+    gameEngine.registerComponent<gengine::component::Motion2D>();
+    gameEngine.registerComponent<gengine::component::HitBoxSquare2D>();
+    gameEngine.registerComponent<gengine::component::HitBoxCircle2D>();
+    gameEngine.registerComponent<gengine::component::Origin2D>();
+
+    gameEngine.registerSystem<Start>();
+    gameEngine.registerSystem<gengine::system::Motion2D>();
+    gameEngine.registerSystem<gengine::system::Collision2D>();
+    gameEngine.registerSystem<HandleCollision>();
     gameEngine.registerSystem<gengine::system::AutoMainLoop>();
     gengine::interface::Internal interface(gameEngine, driverEngine);
     interface.run();
