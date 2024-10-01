45b9b08c722e9fbf3f926d53bb5cfa7086862794
Popochounet <adrien.audiard@epitech.eu>
2024-10-01 10:53:32 +0200

    updated libdev: component => Drawable (zIndex) & system => Draw refacto to display sorted Drawable

---
+++ include/GEngine/libdev/components/driver/output/Drawable.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-LYN-5-1-rtype-basile.fouquet
+** File description:
+** Drawable.hpp
+*/
+
+#pragma once
+
+#include <cstdlib>
+
+#include "GEngine/libdev/Component.hpp"
+
+namespace gengine::component::driver::output {
+struct Drawable : public gengine::Component {
+    std::size_t zIndex;
+    Drawable(std::size_t zIndex) : zIndex(zIndex) {}
+};
+} // namespace gengine::component::driver::output
---
+++ include/GEngine/libdev/systems/events/RenderLoop.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-LYN-5-1-rtype-basile.fouquet
+** File description:
+** RenderLoop.hpp
+*/
+
+#pragma once
+
+#include "GEngine/libdev/System.hpp"
+
+namespace gengine::system::event {
+struct RenderLoop : public Event {
+    float deltaTime;
+    RenderLoop(float &deltaTime) : deltaTime(deltaTime) {}
+    RenderLoop(float &&deltaTime) : deltaTime(deltaTime) {}
+};
+
+struct StopRenderLoop : public Event {};
+} // namespace gengine::system::event
