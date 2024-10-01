63a800099ec765b173deac478c6c74742ae21ee6
Popochounet <adrien.audiard@epitech.eu>
2024-10-01 10:51:59 +0200

    updated libdev: component => Sprite & system => TextureManager (now takes a string, and not a Texture // FontManager)

--- include/GEngine/libdev/components/driver/output/Sprite.hpp
+++ include/GEngine/libdev/components/driver/output/Sprite.hpp
@@ -14,20 +14,22 @@
 
 #pragma once
 
+#include <limits>
 #include <raylib.h>
+#include <string>
 
 #include "GEngine/libdev/Component.hpp"
 
 namespace gengine::component::driver::output {
 struct Sprite : public gengine::Component {
-    Texture texture; // The texture of the sprite
-    ::Rectangle src; // The portion of the texture to render (for sprite sheets/atlases)
+    std::string txtPath; // The texture of the sprite
+    ::Rectangle src;     // The portion of the texture to render (for sprite sheets/atlases)
     Color tint;
 
-    Sprite(const Texture &texture, const ::Rectangle &src, const Color &tint = RAYWHITE)
-        : texture(texture), src(src), tint(tint) {};
-    Sprite(const Texture &texture, const Color &tint = RAYWHITE) : texture(texture), tint(tint) {
-        src = {0, 0, src.width, src.height};
-    };
+    Sprite(const std::string &txtPath, const ::Rectangle &src, const Color &tint = RAYWHITE)
+        : txtPath(txtPath), src(src), tint(tint) {};
+    Sprite(const std::string &txtPath, const Color &tint = RAYWHITE)
+        : txtPath(txtPath), src({0.f, 0.f, std::numeric_limits<float>().max(), std::numeric_limits<float>().max()}),
+          tint(tint) {};
 };
 } // namespace gengine::component::driver::output
