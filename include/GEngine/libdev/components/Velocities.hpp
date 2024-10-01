ca97c6a79139a8f8aab45c79b970fdb422ac016c
Popochounet <adrien.audiard@epitech.eu>
2024-10-01 11:03:36 +0200

    updated libdev: component => Motions are now Velocities to avoid confusion with the homonym system

--- include/GEngine/libdev/components/Velocities.hpp
+++ include/GEngine/libdev/components/Velocities.hpp
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2024
 ** B-CPP-500-LYN-5-1-rtype-basile.fouquet
 ** File description:
-** Motions.hpp
+** Velocities.hpp
 */
 
 #pragma once
@@ -10,13 +10,13 @@
 #include "GEngine/libdev/Component.hpp"
 
 namespace gengine::component {
-struct Motion2D : public Component {
+struct Velocity2D : public Component {
     float x, y;
-    Motion2D(float x, float y) : x(x), y(y){};
+    Velocity2D(float x, float y) : x(x), y(y) {};
 };
 
-struct Motion3D : public Component {
+struct Velocity3D : public Component {
     float x, y, z;
-    Motion3D(float x, float y, float z) : x(x), y(y), z(z){};
+    Velocity3D(float x, float y, float z) : x(x), y(y), z(z) {};
 };
 } // namespace gengine::component
