88e53e44b9ab9c70bd72dab4bf717d5d63f6c013
b3W <basile.fouquet@epitech.eu>
2024-10-02 15:49:27 +0200

    fixed templates: macos compile
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

--- r-type/client/main.cpp
+++ r-type/client/main.cpp
@@ -5,52 +5,52 @@
 ** main.cpp
 */
 
-#include "GEngine/driver/Engine.hpp"
-#include "GEngine/game/Engine.hpp"
-#include "GEngine/interface/Internal.hpp"
-#include "GEngine/libdev/systems/MainLoop.hpp"
-#include "GEngine/libdev/systems/driver/input/KeyboardCatcher.hpp"
-#include "GEngine/libdev/systems/driver/output/Draw.hpp"
-#include "GEngine/libdev/systems/driver/output/RenderWindow.hpp"
+// #include "GEngine/driver/Engine.hpp"
+// #include "GEngine/game/Engine.hpp"
+// #include "GEngine/interface/Internal.hpp"
+// #include "GEngine/libdev/systems/MainLoop.hpp"
+// #include "GEngine/libdev/systems/driver/input/KeyboardCatcher.hpp"
+// #include "GEngine/libdev/systems/driver/output/Draw.hpp"
+// #include "GEngine/libdev/systems/driver/output/RenderWindow.hpp"
 
-class Start : public gengine::System<Start> {
-public:
-    void init(void) override {
-        subscribeToEvent<gengine::system::event::StartEngine>(&Start::onStartEngine);
-    }
+// class Start : public gengine::System<Start> {
+// public:
+//     void init(void) override {
+//         subscribeToEvent<gengine::system::event::StartEngine>(&Start::onStartEngine);
+//     }
 
-    void onStartEngine(gengine::system::event::StartEngine &e) {
-        // spawnEntity(gengine::component::driver::output::Window(1080, 720, "window"),
-        //             gengine::component::driver::output::Color(RED));
-        spawnEntity(gengine::component::driver::output::Rectangle(0, 0, 50, 50),
-                    gengine::component::driver::output::Color(RED));
-        spawnEntity(gengine::component::driver::output::Rectangle(500, 500, 50, 50),
-                    gengine::component::driver::output::Color(BLACK));
-    }
-};
+//     void onStartEngine(gengine::system::event::StartEngine &e) {
+//         // spawnEntity(gengine::component::driver::output::Window(1080, 720, "window"),
+//         //             gengine::component::driver::output::Color(RED));
+//         spawnEntity(gengine::component::driver::output::Rectangle(0, 0, 50, 50),
+//                     gengine::component::driver::output::Color(RED));
+//         spawnEntity(gengine::component::driver::output::Rectangle(500, 500, 50, 50),
+//                     gengine::component::driver::output::Color(BLACK));
+//     }
+// };
 
-class Logger : public gengine::System<Logger> {
-public:
-    void init(void) override {
-        subscribeToEvent<gengine::system::driver::input::KeyAEvent>(&Logger::onKeyA);
-    }
+// class Logger : public gengine::System<Logger> {
+// public:
+//     void init(void) override {
+//         subscribeToEvent<gengine::system::driver::input::KeyAEvent>(&Logger::onKeyA);
+//     }
 
-    void onKeyA(gengine::system::driver::input::KeyAEvent &e) {
-        switch (e.state) {
-        case gengine::system::driver::input::InputState::RELEASE:
-            std::cout << "release" << std::endl;
-            break;
-        case gengine::system::driver::input::InputState::PRESSED:
-            std::cout << "pressed" << std::endl;
-            break;
-        case gengine::system::driver::input::InputState::DOWN:
-            std::cout << "down" << std::endl;
-            break;
-        default:
-            break;
-        }
-    }
-};
+//     void onKeyA(gengine::system::driver::input::KeyAEvent &e) {
+//         switch (e.state) {
+//         case gengine::system::driver::input::InputState::RELEASE:
+//             std::cout << "release" << std::endl;
+//             break;
+//         case gengine::system::driver::input::InputState::PRESSED:
+//             std::cout << "pressed" << std::endl;
+//             break;
+//         case gengine::system::driver::input::InputState::DOWN:
+//             std::cout << "down" << std::endl;
+//             break;
+//         default:
+//             break;
+//         }
+//     }
+// };
 
 // #include "GEngine/libdev/systems/events/Native.hpp"
 
@@ -108,19 +108,19 @@ int main(void) {
     }
 
     Network::NET::stop();
-    gengine::game::Engine gameEngine;
-    gengine::driver::Engine driverEngine;
+    // gengine::game::Engine gameEngine;
+    // gengine::driver::Engine driverEngine;
 
-    gameEngine.registerComponent<gengine::component::driver::output::Window>();
-    gameEngine.registerComponent<gengine::component::driver::output::Rectangle>();
-    gameEngine.registerComponent<gengine::component::driver::output::Color>();
+    // gameEngine.registerComponent<gengine::component::driver::output::Window>();
+    // gameEngine.registerComponent<gengine::component::driver::output::Rectangle>();
+    // gameEngine.registerComponent<gengine::component::driver::output::Color>();
 
-    gameEngine.registerSystem<Start>();
-    gameEngine.registerSystem<gengine::system::driver::input::KeyboardCatcher>();
-    gameEngine.registerSystem<Logger>();
-    gameEngine.registerSystem<gengine::system::AutoMainLoop>();
-    gameEngine.registerSystem<gengine::system::driver::output::RenderWindow>();
-    gameEngine.registerSystem<gengine::system::driver::output::Draw>();
-    gengine::interface::Internal interface(gameEngine, driverEngine);
-    interface.run();
+    // gameEngine.registerSystem<Start>();
+    // gameEngine.registerSystem<gengine::system::driver::input::KeyboardCatcher>();
+    // gameEngine.registerSystem<Logger>();
+    // gameEngine.registerSystem<gengine::system::AutoMainLoop>();
+    // gameEngine.registerSystem<gengine::system::driver::output::RenderWindow>();
+    // gameEngine.registerSystem<gengine::system::driver::output::Draw>();
+    // gengine::interface::Internal interface(gameEngine, driverEngine);
+    // interface.run();
 }
