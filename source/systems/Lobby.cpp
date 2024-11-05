753d2b8845b54944472c37e53a426178dda09bab
Popochounet <adrien.audiard@epitech.eu>
2024-11-05 23:24:46 +0100

    fixed rtype: player respawning during GameOver

--- source/systems/Lobby.cpp
+++ source/systems/Lobby.cpp
@@ -52,13 +52,16 @@ void Lobby::checkPlayersReady(gengine::system::event::GameLoop &e) {
     if (m_started || !m_playersInLobby.size())
         return;
 
-    respawnPlayers(3);
+    auto &states = getComponents<component::GameState>();
+    for (auto &[e, state] : states) {
+        if (state == component::GameState::LOBBY)
+            respawnPlayers(3);
+    }
     for (auto &[uuid, infos] : m_playersInLobby)
         if (!infos.second)
             return;
-    publishEvent(event::StartGame());
-    auto &states = getComponents<component::GameState>();
     auto &netsends = getComponents<geg::component::network::NetSend>();
+    publishEvent(event::StartGame());
     for (auto [e, state, netsend] : gengine::Zip(states, netsends)) {
         state = component::GameState::GAME;
         netsend.update();
@@ -67,10 +70,8 @@ void Lobby::checkPlayersReady(gengine::system::event::GameLoop &e) {
 }
 
 void Lobby::onGameOver(event::GameOver &e) {
-    if (!m_started) {
-        respawnPlayers(3);
+    if (!m_started)
         return;
-    }
     m_started = false;
     for (auto &[uuid, infos] : m_playersInLobby)
         infos.second = false;
@@ -87,7 +88,7 @@ void Lobby::onNewRemoteLocal(gengine::interface::event::NewRemoteLocal &e) {
     m_playersInLobby.emplace(e.uuid, std::make_pair(playerNb, false));
     if (m_started)
         return;
-    spawnPlayer(e.uuid, playerNb, 3);
+    spawnPlayer(e.uuid, playerNb, 4);
 }
 
 void Lobby::spawnPlayer(const uuids::uuid &remoteUUID, size_t playerNb, size_t lifes) {
